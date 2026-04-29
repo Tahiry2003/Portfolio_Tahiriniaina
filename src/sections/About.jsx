f3c9d62dffea0cb66f38138b2ff764db2ae19e31
Tahiry2003 <tahiriniainaarson4@gmail.com>
2026-04-29 10:30:21 +0300

    ajout info About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,125 @@
 function About() {
   return (
-    <section id="about" className="h-screen flex items-center justify-center">
-      <h1 className="text-6xl font-bold text-secondary">About</h1>
+    <section id="about" className="min-h-screen flex items-center">
+      <div className="max-w-7xl mx-auto w-full px-6">
+
+        <h1 className="text-6xl font-bold text-secondary italic mb-10">
+          01. A propos
+        </h1>
+
+        <div className="w-full">
+          <div className="max-w-7xl mx-auto">
+            <h2 className="text-3xl md:text-4xl font-bold text-primary italic mb-6">
+              Qui suis-je ?
+            </h2>
+
+            <p className="text-secondary leading-relaxed text-2xl font-semibold">
+              Développeur passionné par la conception de solutions digitales,
+              je me spécialise dans le développement Web, Mobile ainsi que
+              dans la création de systèmes complexes comme les ERP et les SaaS.
+              <br /><br />
+              J’adopte une approche centrée sur l’architecture logicielle afin de
+              concevoir des applications fiables, évolutives et adaptées aux besoins
+              métiers les plus exigeants.
+              <br /><br />
+              De l’analyse des besoins jusqu’au déploiement, j’interviens sur
+              l’ensemble du cycle de vie des produits afin de transformer des idées
+              en solutions concrètes, performantes et intuitives.
+            </p>
+          </div>
+        </div>
+
+        <div className="mt-20">
+          <h2 className="text-3xl md:text-4xl font-bold text-primary italic mb-8">
+            Expertises Techniques
+          </h2>
+
+<div className="grid md:grid-cols-3 gap-6">
+
+  {/* Frontend */}
+  <div className="p-6 text-center">
+    <h3 className="text-3xl font-bold text-primary mb-8">
+      Frontend
+    </h3>
+
+    <ul className="flex flex-wrap gap-3 justify-start font-semibold text-lg">
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        React
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        Vue.js
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        Laravel
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        HTML
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        CSS
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        Tailwind CSS
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse ">
+        TypeScript
+      </li>
+    </ul>
+  </div>
+
+  {/* Backend */}
+  <div className="p-6 text-center">
+    <h3 className="text-3xl font-bold text-primary mb-8">
+      Backend
+    </h3>
+
+    <ul className="flex flex-wrap gap-3 justify-start font-semibold text-lg">
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        Node.js
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        Express
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        API REST
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        PostgreSQL
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        Architecture MVC
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        Architecture 3tiers
+      </li>
+    </ul>
+  </div>
+
+  {/* Systèmes */}
+  <div className="p-6 text-center">
+    <h3 className="text-3xl font-bold text-primary mb-8">
+      Skills
+    </h3>
+
+    <ul className="flex flex-wrap gap-3 justify-start font-semibold text-lg">
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        Gestion de projet
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        Conception
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        UML
+      </li>
+      <li className="bg-gray-100 text-gray-800 px-6 py-3 rounded-lg border-2 border-collapse">
+        2TUP
+      </li>
+    </ul>
+  </div>
+
+</div>        </div>
+
+      </div>
     </section>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,8 +4,7 @@ function Hero() {
   return (
     <section id="hero" className="h-screen flex items-center">
       <div className="max-w-7xl mx-auto w-full px-6 flex flex-col md:flex-row items-center justify-between gap-10">
-        
-        {/* Texte (gauche) */}
+
         <div className="flex-1">
           <h1 className="text-3xl md:text-4xl font-bold text-secondary space-y-4">
             <span>Bienvenue, Je suis</span>
@@ -20,16 +19,15 @@ function Hero() {
           </h1>
 
           <div className="mt-6 flex items-center space-x-2">
-            <button className="hidden md:block bg-primary text-white px-6 py-3 font-semibold hover:opacity-90 transition">
+            <button className="bg-primary text-white px-6 py-3 font-semibold hover:opacity-90 transition">
               Me contacter
             </button>
-            <button className="hidden md:block bg-primary text-white px-6 py-3 font-semibold hover:opacity-90 transition">
+            <button className="bg-primary text-white px-6 py-3 font-semibold hover:opacity-90 transition">
               Voir mes projets
             </button>
           </div>
         </div>
 
-        {/* Photo (droite) */}
         <div className="flex-1 flex justify-center">
           <div className="relative">
 
@@ -44,22 +42,16 @@ function Hero() {
               <Code size={16} /> Développeur Web
             </span>
 
-            <span className="absolute top-16 -right-20 flex items-center gap-2 
-            bg-white/80 backdrop-blur-md px-4 py-2 text-sm rounded-full shadow-lg
-            hover:scale-110 transition">
+            <span className="absolute top-16 -right-20 flex items-center gap-2 bg-white/80 backdrop-blur-md px-4 py-2 text-sm rounded-full shadow-lg hover:scale-110 transition">
               <Atom size={16} className="text-blue-500" /> React
             </span>
 
-            <span className="absolute -bottom-20 left-16 flex items-center gap-2 
-            bg-white/80 backdrop-blur-md px-4 py-2 text-sm rounded-full shadow-lg
-            hover:scale-110 transition">
-              <Braces size={16} className="text-yellow-500" /> JavaScript
+            <span className="absolute bottom-16 -right-16 flex items-center gap-2 bg-white/80 backdrop-blur-md px-4 py-2 text-sm rounded-full shadow-lg hover:scale-110 transition">
+              <Rocket size={16} className="text-purple-500" /> Full Stack
             </span>
 
-            <span className="absolute bottom-16 -right-16 flex items-center gap-2 
-            bg-white/80 backdrop-blur-md px-4 py-2 text-sm rounded-full shadow-lg
-            hover:scale-110 transition">
-              <Rocket size={16} className="text-purple-500" /> Full Stack
+            <span className="absolute -bottom-20 left-16 flex items-center gap-2 bg-white/80 backdrop-blur-md px-4 py-2 text-sm rounded-full shadow-lg hover:scale-110 transition">
+              <Braces size={16} className="text-yellow-500" /> JavaScript
             </span>
 
           </div>
